357663145d9c8ab1a0c8ea1010cc25d16c1f2d70
root <eglm@cin.ufpe.br>
2025-08-12 15:03:39 +0000

    teste commit pelo container

--- demo/demo_fcos_inference.py
+++ demo/demo_fcos_inference.py
@@ -10,7 +10,8 @@ import tqdm
 from detectron2.data.detection_utils import read_image
 from detectron2.utils.logger import setup_logger
 
-from predictor import VisualizationDemo
+#from predictor import VisualizationDemo
+from predictor import DefaultPredictor
 from adet.config import get_cfg
 
 # constants
@@ -63,99 +64,30 @@ def get_parser():
     )
     return parser
 
-
-if __name__ == "__main__":
+def main():
     mp.set_start_method("spawn", force=True)
-    args = get_parser().parse_args()
-    logger = setup_logger()
-    logger.info("Arguments: " + str(args))
 
+    args = get_parser().parse_args()
+    
+    # create the config
     cfg = setup_cfg(args)
 
-    demo = VisualizationDemo(cfg)
+    # Instantiate the predictor
+    demo = DefaultPredictor(cfg)
 
-    if args.input:
-        if os.path.isdir(args.input[0]):
-            args.input = [os.path.join(args.input[0], fname) for fname in os.listdir(args.input[0])]
-        elif len(args.input) == 1:
-            args.input = glob.glob(os.path.expanduser(args.input[0]))
-            assert args.input, "The input path(s) was not found"
-        for path in tqdm.tqdm(args.input, disable=not args.output):
-            # use PIL, to be consistent with evaluation
-            img = read_image(path, format="BGR")
-            start_time = time.time()
-            predictions, visualized_output = demo.run_on_image(img)
-            
-            logger.info(
-                "{}: detected {} instances in {:.2f}s".format(
-                    path, len(predictions["instances"]), time.time() - start_time
-                )
-            )
+    for path in tqdm.tqdm(args.input, disable=not args.output):
+        
+        img = read_image(path, format="BGR")
+        predictions = demo(img)
 
-            if len(predictions["instances"]) > 0:
-                pred_classes = predictions["instances"].pred_classes.cpu().numpy()
-                pred_boxes = predictions["instances"].pred_boxes.tensor.cpu().numpy()
-                txt_filename = os.path.splitext(path)[0] + ".txt"
-                with open(txt_filename, "w") as f:
-                    for cls, box in zip(pred_classes, pred_boxes):
-                        f.write("{}, {:.3f}, {:.3f}, {:.3f}, {:.3f}\n".format(cls, box[0], box[1], box[2], box[3]))
-
-            if args.output:
-                if os.path.isdir(args.output):
-                    assert os.path.isdir(args.output), args.output
-                    out_filename = os.path.join(args.output, os.path.basename(path))
-                else:
-                    assert len(args.input) == 1, "Please specify a directory with args.output"
-                    out_filename = args.output
-                visualized_output.save(out_filename)
-            else:
-                cv2.imshow(WINDOW_NAME, visualized_output.get_image()[:, :, ::-1])
-                if cv2.waitKey(0) == 27:
-                    break  # esc to quit
-    elif args.webcam:
-        assert args.input is None, "Cannot have both --input and --webcam!"
-        cam = cv2.VideoCapture(0)
-        for vis in tqdm.tqdm(demo.run_on_video(cam)):
-            cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_NORMAL)
-            cv2.imshow(WINDOW_NAME, vis)
-            if cv2.waitKey(1) == 27:
-                break  # esc to quit
-        cv2.destroyAllWindows()
-    elif args.video_input:
-        video = cv2.VideoCapture(args.video_input)
-        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        frames_per_second = video.get(cv2.CAP_PROP_FPS)
-        num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-        basename = os.path.basename(args.video_input)
-
-        if args.output:
-            if os.path.isdir(args.output):
-                output_fname = os.path.join(args.output, basename)
-                output_fname = os.path.splitext(output_fname)[0] + ".mkv"
-            else:
-                output_fname = args.output
-            assert not os.path.isfile(output_fname), output_fname
-            output_file = cv2.VideoWriter(
-                filename=output_fname,
-                # some installation of opencv may not support x264 (due to its license),
-                # you can try other format (e.g. MPEG)
-                fourcc=cv2.VideoWriter_fourcc(*"x264"),
-                fps=float(frames_per_second),
-                frameSize=(width, height),
-                isColor=True,
-            )
-        assert os.path.isfile(args.video_input)
-        for vis_frame in tqdm.tqdm(demo.run_on_video(video), total=num_frames):
-            if args.output:
-                output_file.write(vis_frame)
-            else:
-                cv2.namedWindow(basename, cv2.WINDOW_NORMAL)
-                cv2.imshow(basename, vis_frame)
-                if cv2.waitKey(1) == 27:
-                    break  # esc to quit
-        video.release()
-        if args.output:
-            output_file.release()
-        else:
-            cv2.destroyAllWindows()
+        # save the predictions in a text file
+        if len(predictions["instances"]) > 0:
+            pred_classes = predictions["instances"].pred_classes.cpu().numpy()
+            pred_boxes = predictions["instances"].pred_boxes.tensor.cpu().numpy()
+            txt_filename = os.path.splitext(path)[0] + ".txt"
+            with open(txt_filename, "w") as f:
+                for cls, box in zip(pred_classes, pred_boxes):
+                    f.write("{}, {:.3f}, {:.3f}, {:.3f}, {:.3f}\n".format(cls, box[0], box[1], box[2], box[3]))
+    
+if __name__ == "__main__":
+   main()
